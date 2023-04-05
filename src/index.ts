1ba3201dd22600d714ef0531101cb96bab7d7b32
Vlad Šutov <shxtov@gmail.com>
2023-04-05 06:37:07 +0300

    katana-task - added eslint

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-import express, { Express } from 'express'
+import express, { type Express } from 'express'
 
 const app: Express = express()
 
 app.get('/', (_req, res) => {
   res.send('hello world')
-})  
+})
 
 app.listen(3000, () => {
   console.log('[server]: Server is running at http://localhost:3000')
